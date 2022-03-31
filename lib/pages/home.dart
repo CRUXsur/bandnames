248f69985668a4e0852e9f4a3fcb5d45d0b05f52
CRUXsur <marcojtorrico@hotmail.com>
2022-03-30 23:00:56 -0400

    Interface basica de nuestro HomePage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:band_names/models/band.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metalica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 1),
+    Band(id: '3', name: 'Heroes del Silencio', votes: 2),
+    Band(id: '4', name: 'Bon Jovi', votes: 5),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BandNames',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
       ),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: (context, i) => _bandTile(bands[i]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        elevation: 1,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  //! recibo una banda band de tipo Band!
+  //! ya no recibo un int index!!
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(band.name.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name),
+      trailing: Text(
+        '${band.votes}',
+        style: const TextStyle(fontSize: 20),
+      ),
+      onTap: () {
+        print(band.name);
+      },
     );
   }
 }
